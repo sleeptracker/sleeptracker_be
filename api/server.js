fcbebca3870e7ec9bf856bab0127c528a6c3b00c
slaforsjov <andrewj12763@gmail.com>
2019-09-18 19:25:34 -0600

    building out login routs

--- api/server.js
+++ api/server.js
@@ -1,19 +1,29 @@
+// getting express and hooking it to the server
 const express = require('express');
 const server = express();
+
+// configuring env
 require('dotenv').config();
 
+// pulling in 
 const cors = require('cors');
 const helmet = require('helmet');
 
 const restricted = require('../auth/restricted.js');
 
+const usersRouter = require('../users/users-router.js');
+const authRouter = require('../auth/auth-router.js');
+
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
+server.use('/api/restricted/users', restricted, usersRouter);
+server.use('/api/auth', authRouter);
 
+// sending server response
 server.get('/', (req, res) => {
-    res.status(200).json({ api: 'we are up and running' });
-  });
+  res.status(200).json({ api: 'we are up and running' });
+});
 
 module.exports = server;
--- users/users-model.js
+++ users/users-model.js
@@ -1,4 +1,4 @@
-const db = require('../database/dbConfig.js');
+const db = require('../../backend---Bazen/database/dbConfig.js');
 
 module.exports = {
   find,
