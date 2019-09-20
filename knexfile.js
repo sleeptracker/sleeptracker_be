d495a4d9835ddc069245c905d618e797709b9a6f
slaforsjov <andrewj12763@gmail.com>
2019-09-19 18:06:56 -0600

    fixing db error

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,47 @@
 require('dotenv').config();
 
-
+const dbConnection = process.env.DATABASE_URL;
+//
 module.exports = {
-    development: {
-      client: "sqlite3",
-      useNullAsDefault: true,
-      connection: {
-        filename: "./database/auth.db3"
-      },
-      pool: {
-        afterCreate: (conn, done) => {
-          conn.run("PRAGMA foreign_keys = ON", done);
-        }
-      },
-      migrations: {
-        directory: "./database/migrations"
-      },
-      seeds: {
-        directory: "./database/seeds"
-      }
-    }
-  };
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/users.db3'
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+    useNullAsDefault: true
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: dbConnection,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+    useNullAsDefault: true
+  }
+};
